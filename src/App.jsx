515dbb2337bdd30324ca143cbec8db7502b96476
Kagiso31 <kagisomokou@gmail.com>
2023-10-10 07:07:42 +0200

    Add project files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-indigo-500 text-3xl">Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
